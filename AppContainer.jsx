e26ffd6a07ce19f5e462b2be5a3686a3ea572c93
Aaquiff Rauf <aaquiff@gmail.com>
2018-05-25 22:31:20 +0530

    Bootstrap Navbar with react-router

--- AppContainer.jsx
+++ AppContainer.jsx
@@ -15,16 +15,25 @@ export default class AppContainer extends Component {
     render() {
         return ( <Router>
             <div>
-                <h2>React Router</h2>
-                <ul>
-                    <li><NavLink to={'/wards'}>Wards</NavLink></li>
-                    <li><NavLink to={'/users'}>Users</NavLink></li>
-                </ul>
-                <Switch>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">HIS-Inward</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li><NavLink to={'/wards'}>Wards</NavLink></li>
+                        <li><NavLink to={'/users'}>Users</NavLink></li>
+                    </ul>
+                </div>
+                
+            </nav>
+            <Switch>
                     <Route path='/wards' component={Wards}/>
                     <Route path='/users' component={Users}/>
                 </Switch>
-            </div>
+             </div>   
         </Router>
         )
     }
